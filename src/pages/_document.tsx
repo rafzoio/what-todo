b13b67461b76ab8093dae907ca8cf973051119ba
rafzoio <raphaelzoio@hotmail.com>
2023-08-31 14:03:52 +0100

    Fix typescript error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,13 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  type DocumentContext,
+} from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
@@ -10,16 +16,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
             crossOrigin=""
-          ></link>
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Karla:wght@200;700;800&display=swap"
             rel="stylesheet"
-          ></link>
+          />
         </Head>
         <body>
           <Main />
